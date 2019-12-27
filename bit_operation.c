08c70c3060899a8c2774b129b0f4cb5a96e610de
Nesrovaf <a21663832@gmail.com>
2019-12-27 12:00:44 +0800

    modify declare problem

--- bit_operation.c
+++ bit_operation.c
@@ -2,21 +2,24 @@
 
 //clean the state
 void cleanbuffer(unsigned char *buffer){
-    for (int i = 0; i < 16; i++){
+    int i;
+    for (i = 0; i < 16; i++){
         *(buffer + i) = 0x00;
     }
 }
 
 //copy IV t2 to t1
 void copy(unsigned char *temp1,unsigned char *temp2){
-    for(int i=0;i<16;i++){
+    int i;
+    for(i=0;i<16;i++){
         *(temp1+i) = *(temp2+i); 
     }
 }
 
 //state left shift 8 bit 
 void shift_8(unsigned char *temp){
-    for(int i=0;i<15;i++){
+    int i;
+    for(i=0;i<15;i++){
         *(temp+i) = *(temp+i+1);
     }
 }
@@ -25,7 +28,8 @@ void shift_8(unsigned char *temp){
 void shift_1(unsigned char *temp){
     unsigned char check = 0x80;
     unsigned char temp_8;
-    for(int i=0;i<16;i++){
+    int i;
+    for(i=0;i<16;i++){
         if(i==0){
             *(temp+i) = *(temp+i) << 1;
         }
